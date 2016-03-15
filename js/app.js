6674e90ecb02e40882fe9b8dfb28de3578c8793d
Mark Howard <markhjr@outlook.com>
2016-03-15 16:22:30 -0700

    refactored and cleaned the code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 var App = angular.module("App", []);
 
+// MAIN CONTROLLER
 App.controller("HabitsCtrl", function($scope) {
+  /* CONTROLLER VARS */
   $scope.totalCompleted = 0;
-  $scope.habits = [
-    
-  ];
-
+  $scope.habits = [];
+  
+   /*----------------------
+       UP COUNTER EVENT    
+  ------------------------ */
   $scope.up = function(habit) {
      habit.upCount += 1;
      habit.goal -= 1;
@@ -17,14 +20,24 @@ App.controller("HabitsCtrl", function($scope) {
        $scope.totalCompleted++;
      }
   };
+  /*----------------------
+       DOWN COUNTER EVENT
+  ------------------------ */
   $scope.down = function(habit) {
      habit.downCount += 1;
      habit.goal += 1;
   };
+  
+   /*----------------------
+       DELETE HABIT EVENT    
+  ------------------------ */
   $scope.delete = function(habit) {
     $scope.habits.splice($scope.habits.indexOf(habit), 1);
   };
-
+  
+  /*----------------------
+      CREATE HABIT EVENT     
+  ------------------------ */
   $scope.create = function(name, goal) {
     if(name) {
       $scope.habits.push({
@@ -40,6 +53,4 @@ App.controller("HabitsCtrl", function($scope) {
     $scope.goal = 0;
     console.log($scope.habits)
   };
-  
-  
 });
